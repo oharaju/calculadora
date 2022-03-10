3a53d02f5ace55996a5498afd6e3ee781710e983
Júlia Ohara <juliaohara.br@gmail.com>
2022-03-09 22:39:41 -0300

    insert scripts

---
+++ script.js
@@ -0,0 +1,14 @@
+var nome = "João"
+var notaPrimeiroBimestre = 9
+var notaSegundoBimestre = 4
+var notaTerceiroBimestre = 7
+var notaQuartoBimestre = 2
+var pontoExtra = 2
+var somaDasNotas = notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre
+var notaMedia = somaDasNotas / 4
+var notaFinal = notaMedia + pontoExtra
+
+console.log("Bem vindo " + nome)
+console.log("A somas das suas Notas é " + somaDasNotas)
+console.log("A média das notas é " + notaMedia)
+console.log("Sua nota final é de " + notaFinal)
